9a848d8adfe5c1852188794da29ba1eb87bc16ff
Tabitha Goodall <tg.twitchett@gmail.com>
2017-08-04 12:40:33 +0200

    initial commit (starter files)

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Uno',
+    author: 'Tabitha Goodall',
+  },
+  plugins: [
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+  ]
+}
